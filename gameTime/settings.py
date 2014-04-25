3d4fff9587077b02f124209e831bc5a69a183d36
tayhym <tayhym@gmail.com>
2014-04-25 13:07:14 -0400

    c

--- gameTime/settings.py
+++ gameTime/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'gameTimeApp',
     'south',
-    'boto',
-    'storages',
+    # 'boto',
+    # 'storages',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -177,10 +177,10 @@ print DEBUG
 
 #Storage on S3 settings are stored as os.environs to keep settings.py clean 
 if not DEBUG:
- AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
- AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
- AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
- AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+ AWS_STORAGE_BUCKET_NAME = os.environ[AWS_STORAGE_BUCKET_NAME]
+ AWS_ACCESS_KEY_ID = os.environ[AWS_ACCESS_KEY_ID]
+ AWS_ACCESS_KEY = os.environ[AWS_ACCESS_KEY]
+ AWS_SECRET_ACCESS_KEY = os.environ[AWS_SECRET_ACCESS_KEY]
  STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
  S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
  STATIC_URL = S3_URL
